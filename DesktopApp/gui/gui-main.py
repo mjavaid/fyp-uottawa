0e6cfd2cefac59c2f54af6f00474e5cc7ed278d7
Hamed Abedini <hamededini@gmail.com>
2013-10-29 14:04:02 -0700

    added text to gui-main.py

--- DesktopApp/gui/gui-main.py
+++ DesktopApp/gui/gui-main.py
@@ -1,1 +1,13 @@
 # GUI Main File
+
+from  tkinter import *
+
+#create the window
+root = TK()
+
+#modify root window
+root.title("Simple GUI")
+root.geometry("200x100")
+
+#kick off the event
+root.mainloop()
