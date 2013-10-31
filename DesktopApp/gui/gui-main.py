eee8f2ecdf16f526c4aa675e1cff65c6a8bffe15
Hamed Abedini <hamededini@gmail.com>
2013-10-31 07:56:31 -0400

    A GUI example

--- DesktopApp/gui/gui-main.py
+++ DesktopApp/gui/gui-main.py
@@ -1,13 +1,63 @@
-# GUI Main File
+import  tkinter
+import  turtle
+import  sys
 
-from  tkinter import *
+def main():
 
-#create the window
-root = TK()
+    root = tkinter.Tk()
 
-#modify root window
-root.title("Simple GUI")
-root.geometry("200x100")
+    cv = tkinter.Canvas(root, width = 600, height = 600)
+    cv.pack(side = tkinter.LEFT)
+    root.title("Draw")
 
-#kick off the event
-root.mainloop()
+    t = turtle.RawTurtle(cv)
+    screen = t.getscreen()
+    screen.setworldcoordinates(0,0,600,600)
+
+    frame = tkinter.Frame(root)
+    frame.pack(side = tkinter.RIGHT, fill = tkinter.BOTH)
+    screen.tracer(0)
+
+    def quitHandler():
+        print("GoodBye")
+        sys.exit(0)
+    quitButton = tkinter.Button(frame, text = "Quit", command = quitHandler)
+    quitButton.pack()
+
+    textLab = tkinter.Label(frame, text = "Text to write")
+    textLab.pack()
+    
+    textVar = tkinter.StringVar()
+    textVar.set("Hello World")
+    textEntry = tkinter.Entry(frame, textvariable = textVar)
+    textEntry.pack()
+
+    def writeHandler():
+        t.write(textVar.get())
+        
+    writeButton = tkinter.Button(frame, text = "Write text", command = writeHandler)
+    writeButton.pack()
+
+
+    def clickHandler(x,y):
+        t.goto(x,y)
+        screen.update()
+
+    screen.onclick(clickHandler)
+
+    def dragHandler(x,y):
+        t.goto(x,y)
+        screen.update()
+
+    t.ondrag(dragHandler)
+    
+
+
+    tkinter.mainloop()
+
+
+
+
+
+main()
+    
