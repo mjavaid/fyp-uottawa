284cf83b62f1241a91978efa0ad3f06caeed20d1
Muhammad Javaid <mjavaid@cisco.com>
2013-10-31 17:44:41 -0400

    Updated the gui-main file with a class based architecture. Previous code still available though commented out.

--- DesktopApp/gui/gui-main.py
+++ DesktopApp/gui/gui-main.py
@@ -1,7 +1,7 @@
 from tkinter import *
 import sys
 
-mGui = Tk()
+"""mGui = Tk()
 hamed = StringVar()
 #setting up the window to the size we want 
 mGui.geometry("450x450")
@@ -35,6 +35,45 @@ menubar.add_cascade(label = "About", menu = fileMenu)
 mGui.config(menu = menubar)
 
 #on a windows machine you need to put this, on linux you don't need to do it
-mGui.mainloop()
+mGui.mainloop()"""
 
 
+DEFAULT_TITLE = "DesktopApp"            
+DIMENSIONS, TITLE = 0, 1
+DEFAULT_DIMENSIONS = "450x450"
+
+class GUI:
+    deskApp = None
+
+    def __init__(self, ARGS):
+        self.deskApp = Tk()
+        self.constructGUI(ARGS)
+
+    def constructGUI(self, ARGS):
+        # Set Title
+        if ARGS[TITLE]==None: self.deskApp.title(DEFAULT_TITLE)
+        else: self.deskApp.title(ARGS[self.TITLE])
+        # Set Dimensions
+        if ARGS[DIMENSIONS]==None: self.deskApp.geometry(DEFAULT_DIMENSIONS)
+        else: self.deskApp.geometry(ARGS[DIMENSIONS])
+        # Create Menu
+        menu = Menu(self.deskApp)
+        fileMenu = Menu(menu, tearoff=0)
+        menu.add_cascade(label="File", menu=fileMenu)
+        menu.add_cascade(label="Edit", menu=fileMenu)
+        menu.add_cascade(label="About", menu=fileMenu)
+        self.deskApp.config(menu=menu)
+        # Create UI Buttons
+        canvas = Canvas(self.deskApp, height=35, width=450, bg="lightblue")
+        canvas.pack()
+        scanButton = Button(canvas, text="Scan")
+        scanButton.place(x=10,y=7)
+        uploadButton = Button(canvas, text="Upload")
+        uploadButton.place(x=60,y=7)
+
+    def runGUI(self):
+        """Hangs the Python CLI if invoked"""
+        self.deskApp.mainloop()
+
+gui = GUI([None, None])
+#gui.runGUI()
