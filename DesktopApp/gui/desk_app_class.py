798980d1be6fa34f6efe4fa5bf26ebe1afead1be
Hamed Abedini <hamededini@gmail.com>
2013-11-09 15:53:25 -0500

    Changed the design of the gui - no functionality changes

--- DesktopApp/gui/desk_app_class.py
+++ DesktopApp/gui/desk_app_class.py
@@ -2,11 +2,11 @@ from tkinter import *
 from tkinter import messagebox
 import sys
 
-DEFAULT_TITLE = "DesktopApp"            
+DEFAULT_TITLE = "DesktopApp"
 DIMENSIONS, TITLE = 0, 1
 DEFAULT_DIMENSIONS = "450x450"
 
-class GUI:
+class GUI():
     deskApp = None
 
     def __init__(self, ARGS):
@@ -25,7 +25,9 @@ class GUI:
         # Create Menu
         menu = Menu(self.deskApp)
         fileMenu = Menu(menu, tearoff=0)
+        fileMenu.add_command(label="Connect",command=self.connectHandler)
         fileMenu.add_command(label="Upload", command=self.uploadHandler)
+        fileMenu.add_command(label="Terminate", command=self.terminateHandler)
         fileMenu.add_command(label="Exit", command=self.quitHandler)
         menu.add_cascade(label="File", menu=fileMenu)
         editMenu = Menu(menu, tearoff=0)
@@ -35,39 +37,46 @@ class GUI:
         menu.add_cascade(label="Help", menu=helpMenu)
         self.deskApp.config(menu=menu)
         # Create UI Buttons
-        canvas1 = Canvas(self.deskApp, height=35, width=450, bg="lightblue")
-        canvas1.place(x=0,y=0)
-        canvas2 = Canvas(self.deskApp, height=35, width=450, bg ="lightgray")
-        canvas2.place(x=0,y=35)
-        scanButton = Button(canvas1, text="Scan", command=self.scanHandler)
-        scanButton.place(x=10,y=5)
-        uploadButton = Button(canvas1, text="Upload", command=self.uploadHandler)
-        uploadButton.place(x=60,y=5)
-        executeButton = Button(canvas2, text="Execute", command=self.executeHandler)
-        executeButton.place(x=260,y=6)
-        entry = Entry(canvas2, width=40).place(x=10,y=8)
+        canvas1 = Canvas(self.deskApp, height=65, width=450, bg="darkblue")
+        canvas1.pack()
+        canvas2 = Canvas(self.deskApp, height=32, width=450, bg ="lightgray")
+        canvas2.pack()
+        connectbutton = Button(canvas1, text="Connect", width = 6, fg = "darkgreen", command=self.connectHandler)
+        connectbutton.place(x=10,y=5)
+        terminatebutton = Button(canvas1, text="Terminate", fg = "darkred", command=self.terminateHandler)
+        terminatebutton.place(x=70,y=5)
+        uploadButton = Button(canvas1, text="Upload",width = 6, command=self.uploadHandler)
+        uploadButton.place(x=10,y=33)
+        executeButton = Button(canvas2, text="Execute", bg = "green", command=self.executeHandler)
+        executeButton.place(x=350,y=5)
+        entry = Entry(canvas2, width=50).place(x=10,y=8)
 
     def runGUI(self):
         """Hangs the Python CLI if invoked"""
         self.deskApp.mainloop()
-    
+
     def aboutHandler(self):
         messagebox.showinfo(title="About", message="About Message")
         return
-    
+
     def quitHandler(self):
         choice = messagebox.askyesno(title="Quit", message="Exit Program?")
         if choice != 0: self.deskApp.destroy()
         return
-    
-    def scanHandler(self):
+
+    def connectHandler(self):
         messagebox.showinfo(title="Scanning", message="404: Robot Not Found")
         return
-    
+
+
+    def terminateHandler(self):
+        messagebox.showinfo(title="Terminating", message="404: Not connection to terminate")
+        return
+
     def uploadHandler(self):
         messagebox.showinfo(title="Upload", message="Upload File")
         return
-    
+
     def executeHandler(self):
         messagebox.showinfo(title="Execute", message="Executing Command")
         return
