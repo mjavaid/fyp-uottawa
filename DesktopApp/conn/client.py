2d4270c9f0dc56c9da5134fe3baf8862ddb6e1bf
Rashed Albadi <albadi.rashed@gmail.com>
2013-11-30 22:58:41 -0500

    new client fixed close and no data

--- DesktopApp/conn/client.py
+++ DesktopApp/conn/client.py
@@ -14,12 +14,20 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT)) #double (()) coz connect takes 1 para
 
 while 1:
+		
         MESSAGE = raw_input("Ur message: ")
-        s.send(MESSAGE)
-        print ("watting...")
-        data = s.recv(BUFFER_SIZE)
-
-        print("Reply: "),data
+        c = "close"
+    
+        if MESSAGE !="close":
+        	s.send(MESSAGE)
+        	print ("watting...")
+        	data = s.recv(BUFFER_SIZE)
+        	print("Reply: "),data
+        elif c == MESSAGE:
+        	print "server closed"
+        	break
+        else:
+        	print "not data interd"
 	
 s.close()
 
