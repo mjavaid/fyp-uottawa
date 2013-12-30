4a2b317ce7419b473d78bf07f175cead119d7ef1
Muhammad Javaid <mjavaid@cisco.com>
2013-12-30 11:33:56 -0500

    Modified the app.py file so that it only runs for Python 2.7 and prompts the user if the incorrect version of Python is used

--- DesktopApp/applibs/app.py
+++ DesktopApp/applibs/app.py
@@ -1,5 +1,11 @@
 import gui
+from sys import version_info
 
 if __name__ == "__main__":
-    app = gui.createApp([None, None])
-    app.runGUI()
+    req_version = (2,7)
+    cur_version = version_info
+    if req_version <= cur_version:
+        app = gui.createApp([None, None])
+        app.runGUI()
+    else:
+        print "Python 2.7 required to run application!"
