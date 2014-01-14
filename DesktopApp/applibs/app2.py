2acb2f2127ba685a34a93aeb554bb976033ed811
Muhammad Javaid <mjavaid@cisco.com>
2014-01-14 12:22:31 -0500

    Multiple Changes: > Initial attempt to fix repo structure > Added mapping.py and rebotinterface.py files > Updated the app2.py file to become closer to production code

--- DesktopApp/applibs/app2.py
+++ DesktopApp/applibs/app2.py
@@ -3,9 +3,14 @@
 # Created: 1/10/14
 # Auther: Muhammad Javaid
 
-from tkinter import *
-from tkinter import ttk
-from tkinter import filedialog
+try:
+    from tkinter import *
+    from tkinter import ttk
+    from tkinter import filedialog
+except ImportError:
+    from Tkinter import *
+    import ttk
+    import tkFileDialog as filedialog
 
 lastx, lasty = 0, 0
 
@@ -20,81 +25,129 @@ def addLine(event):
 
 def uploadHandler(event=None):
     print("Upload Handler")
-    filename = filedialog.askopenfilename()
+    filename = filedialog.askopenfilename(filetypes=(('3D EMR', '*.emr'),('All Files', '*.*')))
     print(filename)
 
 def executeHandler(event=None):
     print("Execute Handler")
+    #plotMap()
 
 def connectHandler(event=None):
     print("Connect Handler")
-    plot2DCanvas.config(width=700)
+    #print(plot2DCanvas.winfo_width())
+    #plot2DCanvas.config(width=(plot2DCanvas.winfo_width()+10))
 
 def aboutHandler(event=None):
     aboutWindow = Toplevel()
     aboutWindow.title('About')
     aboutWindow.resizable(FALSE, FALSE)
 
-app = Tk()
-app.title('3D-EMR Desktop Application')
-app.option_add('*tearOff', FALSE)
-app.resizable(FALSE, FALSE)
+class COORDINATES:
+    x = None
+    y = None
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
-menubar = Menu(app)
-menu_file = Menu(menubar)
-menu_file.add_command(label='Upload', command=uploadHandler)
-menu_file.add_command(label='Connect', command=connectHandler)
-menu_file.add_separator()
-menu_file.add_command(label='Quit', command=app.quit)
+def plotMap(event=None):
+    for i in range(20):
+        coord = COORDINATES(i*10, i*100)
+        addLine(coord)
 
-menu_help = Menu(menubar)
-menu_help.add_command(label='About', command=aboutHandler)
+def createGUI(app=None):
+    if app == None:
+        print("No App Provided")
+        return
 
-menubar.add_cascade(menu=menu_file, label='File')
-menubar.add_cascade(menu=menu_help, label='Help')
+    menubar = Menu(app)
+    menu_file = Menu(menubar)
+    menu_file.add_command(label='Upload', command=uploadHandler)
+    menu_file.add_command(label='Connect', command=connectHandler)
+    menu_file.add_separator()
+    menu_file.add_command(label='Quit', command=app.quit)
 
-app.configure(menu=menubar)
+    menu_help = Menu(menubar)
+    menu_help.add_command(label='About', command=aboutHandler)
 
-window = ttk.Frame(app)
-actionButtonsFrame = ttk.Frame(window, padding=(5, 5))
-executeOptionsFrame = ttk.Frame(window, padding=(5, 5))
+    menubar.add_cascade(menu=menu_file, label='File')
+    menubar.add_cascade(menu=menu_help, label='Help')
 
-canvasFrame = ttk.Frame(window)
-plot2DCanvas = Canvas(canvasFrame, width=1000, height=1000)
-hbar = Scrollbar(canvasFrame, orient=HORIZONTAL, command=plot2DCanvas.xview)
-hbar.grid(column=0, row=1, sticky=(E, W))
-vbar = Scrollbar(canvasFrame, orient=VERTICAL, command=plot2DCanvas.yview)
-vbar.grid(column=1, row=0, sticky=(N, S))
-plot2DCanvas.config(xscrollcommand=hbar.set, yscrollcommand=vbar.set)
-plot2DCanvas.grid_columnconfigure(0, weight=1)
+    app.configure(menu=menubar)
 
-#uploadImg = PhotoImage(file='../resources/images/upload.gif')
-uploadBtn = ttk.Button(actionButtonsFrame, text='Upload', command=uploadHandler)
-connectBtn = ttk.Button(actionButtonsFrame, text='Connect', command=connectHandler)
+    window = ttk.Frame(app)
+    actionButtonsFrame = ttk.Frame(window, padding=(5, 5))
+    executeOptionsFrame = ttk.Frame(window, padding=(5, 5))
 
-executeBtn = ttk.Button(executeOptionsFrame, text='Execute', command=executeHandler)
-cmd = StringVar()
-commandEntry = ttk.Entry(executeOptionsFrame, textvariable=cmd)
+    canvasFrame = ttk.Frame(window)
+    h = ttk.Scrollbar(canvasFrame, orient=HORIZONTAL)
+    v = ttk.Scrollbar(canvasFrame, orient=VERTICAL)
+    plot2DCanvas = Canvas(canvasFrame, scrollregion=(0, 0, 700, 700), width=500, height=500, yscrollcommand=v.set, xscrollcommand=h.set, bg='#f00')
+    h['command'] = plot2DCanvas.xview
+    v['command'] = plot2DCanvas.yview
+    ttk.Sizegrip(canvasFrame).grid(column=1, row=1, sticky=(S,E))
+    plot2DCanvas.grid(column=0, row=0, sticky=(N,S,E,W))
+    h.grid(column=0,row=1,sticky=(W,E))
+    v.grid(column=1,row=0,sticky=(N,S))
+    canvasFrame.grid_columnconfigure(0,weight=1)
+    canvasFrame.grid_rowconfigure(0, weight=1)
 
-window.grid(column=0, row=0, sticky=(N, S, E, W))
-actionButtonsFrame.grid(column=0, row=0, sticky=(E, W))
-ttk.Separator(window, orient=HORIZONTAL).grid(column=0, row=1, sticky=(E, W))
-executeOptionsFrame.grid(column=0, row=2, sticky=(E, W))
-ttk.Separator(window, orient=HORIZONTAL).grid(column=0, row=3, sticky=(E, W))
-canvasFrame.grid(column=0, row=4, sticky=(N, S, E, W))
+    #plot2DCanvas.bind("<Button-1>", xy)
+    #plot2DCanvas.bind("<B1-Motion>", addLine)
 
-plot2DCanvas.grid(column=0, row=0, sticky=(N, S, E, W))
-plot2DCanvas.bind("<Button-1>", xy)
-plot2DCanvas.bind("<B1-Motion>", addLine)
+    """canvasFrame = ttk.Frame(window, width=500, height=500)
+    canvasFrame.rowconfigure(0, weight=1)
+    canvasFrame.columnconfigure(0, weight=1)
 
-uploadBtn.grid(column=0, row=0, sticky=(W))
-connectBtn.grid(column=1, row=0, sticky=(W))
+    plot2DCanvas = Canvas(canvasFrame)
+    plot2DCanvas.grid(column=0, row=0, sticky=(N, S, E, W))
+    scrollb = Scrollbar(canvasFrame, command=plot2DCanvas.yview)
+    scrollb.grid(row=0, column=1, sticky=(N, S, E, W))
+    plot2DCanvas.config(yscrollcommand=scrollb.set)"""
 
-commandEntry.grid(column=0, row=0, sticky=(W))
-executeBtn.grid(column=1, row=0, sticky=(W))
 
-app.bind('<Command-u>', uploadHandler)
-app.bind('<Command-e>', executeHandler)
-app.bind('<Command-r>', connectHandler)
+    """plot2DCanvas = Canvas(canvasFrame, width=500, height=500)
+    hbar = Scrollbar(canvasFrame, orient=HORIZONTAL, command=plot2DCanvas.xview)
+    hbar.grid(column=0, row=1, sticky=(E, W, S))
+    vbar = Scrollbar(canvasFrame, orient=VERTICAL, command=plot2DCanvas.yview)
+    vbar.grid(column=1, row=0, sticky=(N, S, E))
+    plot2DCanvas.config(xscrollcommand=hbar.set, yscrollcommand=vbar.set)
+    plot2DCanvas.grid_columnconfigure(0, weight=1)
+    canvasFrame.grid_columnconfigure(0, weight=1)"""
 
-app.mainloop()
+    #uploadImg = PhotoImage(file='../resources/images/upload.gif')
+    uploadBtn = ttk.Button(actionButtonsFrame, text='Upload', command=uploadHandler)
+    connectBtn = ttk.Button(actionButtonsFrame, text='Connect', command=connectHandler)
+
+    executeBtn = ttk.Button(executeOptionsFrame, text='Execute', command=executeHandler)
+    cmd = StringVar()
+    commandEntry = ttk.Entry(executeOptionsFrame, textvariable=cmd, width=50)
+
+    window.grid(column=0, row=0, sticky=(N, S, E, W))
+    actionButtonsFrame.grid(column=0, row=0, sticky=(E, W))
+    ttk.Separator(window, orient=HORIZONTAL).grid(column=0, row=1, sticky=(E, W))
+    executeOptionsFrame.grid(column=0, row=2, sticky=(E, W))
+    ttk.Separator(window, orient=HORIZONTAL).grid(column=0, row=3, sticky=(E, W))
+    canvasFrame.grid(column=0, row=4, sticky=(N, S, E, W))
+
+    uploadBtn.grid(column=0, row=0, sticky=(W))
+    connectBtn.grid(column=1, row=0, sticky=(W))
+
+    commandEntry.grid(column=0, row=0, sticky=(W))
+    executeBtn.grid(column=1, row=0, sticky=(W))
+
+if __name__ == '__main__':
+    app = Tk()
+    app.title('3D-EMR Desktop Application')
+    app.option_add('*tearOff', FALSE)
+    app.resizable(FALSE, FALSE)
+
+    app.bind('<Command-u>', uploadHandler)
+    app.bind('<Control-u>', uploadHandler)
+    app.bind('<Command-e>', executeHandler)
+    app.bind('<Control-e>', executeHandler)
+    app.bind('<Command-r>', connectHandler)
+    app.bind('<Control-r>', connectHandler)
+
+    createGUI(app)
+
+    app.mainloop()
--- Mapping/2dmapping.py
+++
@@ -1,1 +0,0 @@
-# File for 2D mapping
