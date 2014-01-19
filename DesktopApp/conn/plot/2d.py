8c815afad71eab6c05c091dfd278c049f90dbdb8
Muhammad Javaid <mjavaid@cisco.com>
2014-01-19 09:51:56 -0500

    Added tests for the findNewXAndY function. Errors for values in the 4th quadrant. Fix in next commit.

--- DesktopApp/conn/plot/2d.py
+++ DesktopApp/conn/plot/2d.py
@@ -8,7 +8,7 @@ def findNewXAndY(distance, angle, oldX, oldY):
     RAD_180 = radians(180)
     RAD_360 = radians(360)
     
-    if (angle == 0 and angle <= RAD_180):
+    if (angle >= 0 and angle <= RAD_180):
         return (distance * cos(angle), distance * sin(angle), NO_ERR)
     elif (angle > RAD_180 and angle <= RAD_360):
         return (distance * sin(angle), distance * cos(angle), NO_ERR)
@@ -16,8 +16,9 @@ def findNewXAndY(distance, angle, oldX, oldY):
 	
 if __name__ == "__main__":
     x, y = 0, 0
-    distance = 4
-    angle = radians(300)
-    
-    x, y, ERR = findNewXAndY(distance, angle, x, y)
-    print "x:", x, "y:", y, "ERR:", ERR
+    distance = 2.82842712475
+    angle = radians(45)
+    for i in range(5):
+        x, y, ERR = findNewXAndY(distance, angle, x, y)
+        print "Angle:", degrees(angle), ">> x:", x, "y:", y, "ERR:", ERR
+        angle += radians(90)
