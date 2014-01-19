57646593f1a34eba06f98d45d64aea275e1720ac
Muhammad Javaid <mjavaid@cisco.com>
2014-01-19 09:40:43 -0500

    Removed a few bugs from the 2d.py file. Updated input and output values to better reflect the expected behaviour.

--- DesktopApp/conn/plot/2d.py
+++ DesktopApp/conn/plot/2d.py
@@ -1,25 +1,23 @@
-import matplotlib.pyplot as plt
-import numpy as np 
+#import matplotlib.pyplot as plt
+#import numpy as np
 from math import cos, sin, degrees , radians
 
-INVALID_ARGS = "INVALID_ARGS"
+NO_ERR, INVALID_ARGS = 0, 1
 
-def findNewXAndY(distance, angle):
+def findNewXAndY(distance, angle, oldX, oldY):
     RAD_180 = radians(180)
     RAD_360 = radians(360)
     
     if (angle == 0 and angle <= RAD_180):
-        x, y = leng * cos(angle), distance * sin(angle)
+        return (distance * cos(angle), distance * sin(angle), NO_ERR)
     elif (angle > RAD_180 and angle <= RAD_360):
-        x, y = leng * sin(angle), distance * cos(angle)
-    else:
-        return INVALID_ARGS
-    return (x, y)
+        return (distance * sin(angle), distance * cos(angle), NO_ERR)
+    else: return (x, y, INVALID_ARGS)
 	
 if __name__ == "__main__":
     x, y = 0, 0
     distance = 4
     angle = radians(300)
     
-    x, y = findNewXAndY(distance, angle)
-    print "x:", x, "y:", y
+    x, y, ERR = findNewXAndY(distance, angle, x, y)
+    print "x:", x, "y:", y, "ERR:", ERR
