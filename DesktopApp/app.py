c98835484ba77aaf853a6acdeeb073daba6e9657
Muhammad Javaid <mjavaid@cisco.com>
2014-01-20 08:48:33 -0500

    Added initial app.py code to run the app from main interface.

--- DesktopApp/app.py
+++ DesktopApp/app.py
@@ -1,1 +1,11 @@
-# Main File
+# Main Application File
+
+import sys
+
+sys.path.append('./applibs/')
+
+from gui import APPLICATION
+
+if __name__ == "__main__":
+    app = APPLICATION()
+    app.executeGUI()
