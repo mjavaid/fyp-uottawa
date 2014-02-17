f7cdf59fb6c518fdca0e28178eeeb6b9c3911fec
Rayan Alsubhi <khyal2008@gmail.com>
2014-02-17 15:57:03 -0500

    Added cameraPan.py and tiltingPan.py

---
+++ RobotMobility/tiltingPan.py
@@ -0,0 +1,48 @@
+import Adafruit_BBIO.PWM as PWM
+
+RUNNING = False
+STARTED = False
+
+def stop():
+    print "Stopping Motors...\n"
+    PWM.set_duty_cycle("P9_14",100)
+    #PWM.set_duty_cycle("P9_22",100)
+
+def start():
+    global RUNNING, STARTED
+    RUNNING = True
+    print "Starting Motors...\n"
+    if STARTED:
+        PWM.set_duty_cycle("P9_14",0)
+        #PWM.set_duty_cycle("P9_22",5)
+    else:
+        print "test"
+        STARTED = True
+        PWM.start("P9_14",97,33,0)
+        #PWM.start("P9_14",97,33,1)
+
+if __name__ == "__main__":
+    while True:
+	userInput = str(raw_input("Enter Command: "))
+        userInput = userInput.upper()
+        if userInput == "BREAK":
+            stop()
+            break
+        elif userInput == "START":
+            if RUNNING:
+                print "Already Running!\n" 
+                continue
+            start()
+        elif userInput == "STOP":
+            if not STARTED:
+                print "Start Motors First Doofus!\n"
+                continue
+            if not RUNNING: 
+                print "Already Stopped!\n"
+                continue
+            RUNNING = False
+            stop()
+        else: print "Invalid Command!\n"
+
+
+ 
