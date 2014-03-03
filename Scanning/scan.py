afcd5c349a7681c62d22e4c630659d8ecead4954
Muhammad Javaid <mjavaid@cisco.com>
2014-03-02 23:32:58 -0500

    Algorithm optimized. Calibration for robot required.

--- Scanning/scan.py
+++ Scanning/scan.py
@@ -2,50 +2,34 @@
 # Captures a picture and calculates the brightests pixel on each y-axis
 # pixel row. Returns the result of the calculation to the user.
 """
+import numpy as np
 import cv2.cv as cv
 from math import tan, fabs, asin
-        
-def findLaserCenterByRow(imgRow, rowWidth):
-    THRESHOLD_MIN_PWR = 25
-    THRESHOLD_BLOB_DIFF = 10
-    THRESHOLD_ALLOWED_BLOB_SZ = 20
-    centerPos = 0
-    maxPwr = 0
-    
-    for pos in range(rowWidth):
-        if maxPwr < imgRow[pos]:
-            centerPos = pos
-            maxPwr = imgRow[pos]
-    
-    if maxPwr < THRESHOLD_MIN_PWR: return -1
+import json
 
-    logicPwr, totalPwr = 0.0, 0.0
-    for pos in range(centerPos-10,centerPos+11):
-        currentPwr = 0.0
-        if pos >= 0 and pos < rowWidth:
-            currentPwr = imgRow[pos]
-        logicPwr += currentPwr
-        totalPwr += currentPwr * pos
-    
-    return totalPwr / logicPwr
+THRESHOLD_MIN_PWR = 25
 
 def calcDistanceByPos(x, y, imgWidth):
     OFFSET = -0.03460372108
     GAIN = 0.0015772
-    pfc = x - (imgWidth / 2)
+    pfc = fabs(x - (imgWidth / 2))
     
     distance = 6.1 / tan( pfc * GAIN * OFFSET )
     
-    CENTER_Y = 239
+    CENTER_Y, thetaZ = 239, 0
     if not y == CENTER_Y:
         y = fabs(y - CENTER_Y)
         y *= 0.02645833333
-        theta = asin(y / distance)
-        distance = y / tan( theta )
+        thetaZ = asin(y / distance)
+        distance = y / tan( thetaZ )
     
-    return distance
+    return distance, thetaZ
+
+def findLaserCenterByRow(imgRow=None):
+    if max(imgRow) < THRESHOLD_MIN_PWR or imgRow == None: return -1
+    else: return np.argmax(imgRow)
         
-def take_picture(camIndex=0):
+def take_picture(camIndex=0, thetaX=0):
     capture = cv.CaptureFromCAM(camIndex)
     img = cv.QueryFrame(capture)
     
@@ -56,20 +40,23 @@ def take_picture(camIndex=0):
     cv.SaveImage('output.jpg', img)
     cv.SaveImage('outputG.jpg', imgGray)
     
-    brightestPoints = []
-    for row in range(imgGray.height):
-        rowPoints = []
-        for col in range(imgGray.width):
-            rowPoints.append(imgGray[row, col])
-        brightestPoints.append(findLaserCenterByRow(rowPoints, imgGray.width))
+    openCV2npArr = np.asarray( imgGray[:,:] )
+    brightestPoints = np.apply_along_axis(findLaserCenterByRow, axis=1, arr=openCV2npArr)
     
-    distances = []
+    scanOutput = []
     for y in range(len(brightestPoints)):
-        distances.append(calcDistanceByPos(brightestPoints[y], y, imgGray.width))
-        
-    for i in range(3):
-        print "X:", brightestPoints[i], "Y:", i, "D:", distances[i]
+        distance, thetaZ = calcDistanceByPos(brightestPoints[y], y, imgGray.width)
+        scanOutput.append({
+            'x': brightestPoints[y],
+            'y': y,
+            'distance': distance,
+            'thetaZ': thetaZ,
+            'thetaX': thetaX
+        })
     
+    with open("scan-%s.txt" % thetaX, "w") as outfile:
+        json.dump(scanOutput, outfile, indent=4)
+ 
 if __name__=='__main__':
     take_picture()
     
